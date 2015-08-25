8e07fc986ed95b917998c98706ef086bf59811a2
Christoph Herrmann <developer@dragonprojects.de>
2015-08-25 22:59:18 +0200

    Output format and some formatting

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+// outputformat: text/plain
+header('Content-Type:text/plain');
+
 // give the last cron 15 min time to run
 $interval = getenv('INTERVAL') - (60 * 15);
 
@@ -11,7 +14,7 @@ echo 'latest post:' . "\n";
 echo 'ID: ' . $publish->ID . ', title: ' . $publish->post_title . ', post_date: ' . $publish->post_date . "\n";
 if (strtotime($publish->post_date) >= (time() - $interval)) {
     // found a post in the interval, so nothing to do
-    echo 'Nothing to publish, found current post.' . "\n";
+    echo "\n" . 'Nothing to publish, found current post.' . "\n";
     exit(0);
 }
 
@@ -19,13 +22,13 @@ if (strtotime($publish->post_date) >= (time() - $interval)) {
 $pendings = get_posts(array('numberposts' => 1, 'orderby' => 'post_date', 'order' => 'ASC', 'post_status' => 'pending'));
 if (count($pendings) == 0) {
     // no pending posts available to publish
-    echo 'Nothing to publish, no pending post available.' . "\n";
+    echo "\n" . 'Nothing to publish, no pending post available.' . "\n";
     exit(0);
 }
 $pending = $pendings[0];
 
 // publish the pending post
-echo 'Publish pending post.' . "\n";
+echo "\n" . 'Publish pending post.' . "\n";
 echo 'ID: ' . $pending->ID . ', title: ' . $pending->post_title . "\n";
 $now = time();
 wp_update_post(['ID' => $pending->ID, 'post_status' => 'publish', 'post_date' => date('Y-m-d H:i:s', $now), 'post_date_gmt' => gmdate('Y-m-d H:i:s', $now)]);
